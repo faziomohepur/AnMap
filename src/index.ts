475a762006e03bb2bac6f902f2351fa61af3c09c
faziomohepur <faziomohepur@gmail.com>
2026-09-16 05:11:02 +0700

    q

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export default {
+  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+    return new Response("Hello from ten-worker!", {
+      headers: { "content-type": "text/plain" },
+    });
+  },
+};
